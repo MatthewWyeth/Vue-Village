6fe6cf0c131ed22df9f46d4f55a7032ea7557720
MatthewWyeth <matt.wyeth@live.com>
2019-05-29 01:14:13 +1000

    CORE: removed vuex module structure, overkill for this simple project

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,64 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
-import endTurn from './modules/endTurn';
-import village from './modules/village';
-//import * as actions from './actions';
+import buildings from '../data/buildings';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-   // actions,
-    modules: {
-        village,
-        endTurn
-    }
+	state: {
+		gold: 10000,
+		buildings: {
+			farm: {
+				id: 1, 
+				price: 10,
+				quantity: 0
+			},
+			wall: {
+				id: 2, 
+				price: 10,
+				quantity: 0
+			},
+			house: {
+				id: 3, 
+				price: 10,
+				quantity: 0
+			},
+			tower: {
+				id: 4, 
+				price: 10,
+				quantity: 0
+			},
+		}
+	},
+	mutations : { 
+		'BUY_BUILDING' (state, {quantity, type}) {
+			state.buildings[type].quantity += quantity
+			state.gold -= type.price * quantity;
+		},
+		'SET_BUILDINGS' (state, buildings) {
+			state.buildings = buildings;
+		},
+		'END_TURN' (state) {
+			
+		}
+	},
+	getters : {
+		getVillage: function getVillage(state)  { 
+			return state.buildings
+		},
+		getGold: function getGold(state) {
+		  return state.gold;
+		},
+	},
+	actions: {
+		buyBuilding: ({commit}, data) => {
+			commit('BUY_BUILDING', data);
+		},
+		initBuildings: ({commit}) => {
+			commit('SET_BUILDINGS', buildings);
+		},
+		endTurn: ({commit}) => {
+			commit('END_TURN');
+		}
+	}
 });
